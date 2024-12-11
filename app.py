5a9008bb3793a57e940110e4859bbf1dc6ff0bd1
Lanfish04 <137701874+Lanfish04@users.noreply.github.com>
2024-12-12 00:47:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 app = Flask(__name__)
 
 # Path ke model
-model_path = '/home/my-compute/code-repository/api-backend-capstone/fresee.h5'
+model_path = 'fresee.h5'
 
 # Load model
 model = tf.keras.models.load_model(model_path)
@@ -18,7 +18,6 @@ max_size = 2 * 1024 * 1024
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Ambil file gambar dari request
     img = request.files.get('img')
     
     if not img:
@@ -27,40 +26,49 @@ def predict():
             "message": "Gambar harus ada"
         }), 400
 
-    if len(img.read()) > max_size:
+    # Simpan konten file ke variabel
+    img_content = img.read()
+
+    if len(img_content) > max_size:
         return jsonify({"status": False, "message": "Ukuran gambar terlalu besar"}), 400
 
     try:
-        # Baca file gambar ke dalam stream
-        img_stream = io.BytesIO(img.read())
+        # Baca file gambar dari stream
+        img_stream = io.BytesIO(img_content)
         
         # Muat gambar dari stream dan ubah ukurannya
-        img_path = load_img(img_stream, target_size=(150, 150))
-        
+        img_path = load_img(img_stream, target_size=(160, 160))
+
         # Konversi gambar ke array
         img_array = img_to_array(img_path)
-        
+
         # Normalisasi gambar (skala 0-1)
         img_array = img_array / 255.0
-        
+
         # Tambahkan dimensi batch
         img_array = np.expand_dims(img_array, axis=0)
-        
+
         # Prediksi dengan model
         prediction = model.predict(img_array)
-        
+
         # Interpretasi hasil prediksi
         threshold = 0.5  # Ambang batas biner
-        if prediction[0] <= threshold:
+        if prediction[0][0] <= threshold:
             return jsonify({
                 "status": True,
-                "message": "Buah atau sayur segar",
+                "message": "Produk ini Segar!",
+                "probability": float(prediction[0][0])
+            })
+        elif prediction[0][0] > threshold:
+            return jsonify({
+                "status": False,
+                "message": "Produk ini Tidak Segar!",
                 "probability": float(prediction[0][0])
             })
         else:
             return jsonify({
                 "status": False,
-                "message": "Buah atau sayu tidak segar",
+                "message": "Produk Tidak dikenali.",
                 "probability": float(prediction[0][0])
             })
     except Exception as e:
@@ -68,6 +76,6 @@ def predict():
             "status": "error",
             "message": str(e)
         }), 500
-
+        
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=4500)
